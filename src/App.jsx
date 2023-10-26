ca90205a2af702995afe3c61bade2fb039eab902
Radin6 <nicoramos647@hotmail.com>
2023-10-26 11:42:25 +0200

    smart contract bank added 2

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,9 @@ function App() {
         <Projects>
           <Project
             title={'Web3 Bank Project v1'}
-            href={'#'}
+            href={
+              'https://radin6.github.io/src/pages/bank-project/bank-project.html'
+            }
             text={
               'This is a simple Smart contract and Webpage that simulate a Bank.\
               Back-End: Solidity, Javascript, Front-End: Javascript, HTML and CSS'
